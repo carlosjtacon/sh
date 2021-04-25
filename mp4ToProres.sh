772e531a263289b7cb86b5a0049849ab1f95f97f
Carlos <carlosjtacon@gmail.com>
2021-04-25 12:19:02 +0200

    added m4v

--- mp4ToProres.sh
+++ mp4ToProres.sh
@@ -1,1 +1,2 @@
 for f in *.mp4 ; do ffmpeg -i "$f" -c:v prores_ks -profile:v 3 -c:a pcm_s16le "${f%.mp4}.mov" ; done
+for f in *.m4v ; do ffmpeg -i "$f" -c:v prores_ks -profile:v 3 -c:a pcm_s16le "${f%.m4v}.mov" ; done
